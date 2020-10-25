73e268dd903096322f4e910dfbef6dc100d998d7
Upulie Han <ireshaupulie@gmail.com>
2020-10-26 01:19:02 +0530

    :art: Scaffolded for the Movie entity, Added the Watchlist Controller, Added default Views.

---
+++ source/Watchlist/Watchlist/Controllers/WatchlistController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Watchlist.Controllers
+{
+    public class WatchlistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
