e428d598a7936f7dbf3a2bee476576b8900c58fc
Upulie Han <ireshaupulie@gmail.com>
2020-10-28 00:49:30 +0530

    :art: Added the startbootstrap-sb-admin-2-gh-pages template and changed the home screen to contain the watchlist table. Need to change the other pages on the Dashboard. Need to add the Login partial view to the dashboard or somewhere else. Add the 404 page too.

--- docs/index.md
+++ docs/index.md
@@ -44,4 +44,66 @@ To view the DB created,
 2. To data on each individual tables, right click on the table > View Data
 3. To see the structure of a table, right click on the table > View Code
 
+If you need to use inputs in your forms, specifying the format and the text area inputs easiest way is to use a helper method
+*a0- text input for a model item named Comments
+*a1- the number of rows (8)
+*a2- the number of columns(0 means the box will fill the width of the available space)
+*a3- an anonymous object that represents the HTML attributes applied to the box (the form-control CSS class, an the required attribute)
+@Html.TextAreaFor(model => model.Comments, 8, 0, new {
+    @class = “form-control”, required = “required” })
+
+
+Authentication - The process by which your application verifies a user's identity.
+Authorization - How you decide what parts of your site you will allow users to acess.
+
+To implement authorization,
+Make a list of controllers and controller actions in your application and give each one an access level.
+Eg: 
+All users
+Registered(authenticated) users
+Specific types of registered users
+Specific users
+
+To debug always use Fn+F5. Selecting debug on the menu doesn't work?
+
+wwwroot container holds all the assets of the project (css files,img,js files,lib)
+
+To add a boostrap template,
+1.Extract it
+2.Copy the folders inside it
+3.Paste them inside wwwroot (although there are previous assets inside it (there's not a lot))
+4.Now set up the _Layout.cshtml page so it can be applied to all the other pages in the project .
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+    @RenderBody()
+
+
+    @RenderSection("Scripts", required: false)
+</body>
+</html>
+
+*We need @RenderBody to render the page in response to the request
+*@RenderSection - We need to render a section of code from within any of the pages
+
+5.Now open up the home.html or index.html page from the template folder on an editor.
+6. Copy the head of it to the head of the _Layout page
+7. Copy the body of home.html (up until Content) and put it right above @RenderBody
+8. Skip the Content part on home.html (because @RenderBody will replace that)
+9. Copy the code on home.html after Content (the footer part including the script tags, up until the </body>) and paste it after the previous section
+10. Now since you've changed the relative location of the html pages and the lib files (such as bootstrap files) you need to map them accordingly.
+(Earlier all the assets and sample html files from the template were in the same folder, now they are not (our assets are inside wwwroot and our htmls are inside Views folder))
+11. To map locations, go through the whole Layout file and add "~/" infront of all assets that are looking for a location
+12. Remove the href="" on the Layout that refer to a sample page in the template
+13. @RenderSection.. should be placed right before the ending of the </body> and after the script tags of the template
+5.Then set a sample page to see the template in action.
+6.Now you need to get into every razor page and add the template.
+
+When building the view model for a particular view,
+Although the model is often built in the controller action and passed to the view as a parameter, 
+the type defined in the action must match the type defined in the view with the  @model  command.
+
 
--- source/Watchlist/Watchlist/Controllers/MoviesController.cs
+++ source/Watchlist/Watchlist/Controllers/MoviesController.cs
@@ -6,23 +6,114 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Watchlist.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Watchlist.Models;
 
 namespace Watchlist.Controllers
 {
+    //Only allow registered users to access within the controller
+    //If the user is not logged in, they will be redirected to the Login page
+    [Authorize]
     public class MoviesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         //access the database context through ASP.NET's dependency injection
-        public MoviesController(ApplicationDbContext context)
+        public MoviesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+
+        //getting the current user
+        private Task<ApplicationUser> GetCurrentUserAsync() =>
+            _userManager.GetUserAsync(HttpContext.User);
+
+        //getting the current user
+        [HttpGet]
+        public async Task<string> GetCurrentUserId()
+        {
+            ApplicationUser usr = await GetCurrentUserAsync();
+            return usr?.Id;
+        }
+
 
         // GET: Movies
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Movies.ToListAsync());
+            var userId = await GetCurrentUserId();
+
+            //change the list of Movie objects to MovieViewModel objects
+            var model = await _context.Movies.Select(x =>
+                  new MovieViewModel
+                  {
+                      MovieId = x.Id,
+                      Title = x.Title,
+                      Year = x.Year
+                  }).ToListAsync();
+
+            foreach (var item in model)
+            {
+                var m = await _context.UserMovies.FirstOrDefaultAsync(x =>
+                      x.UserId == userId && x.MovieId == item.MovieId);
+
+                if (m != null)
+                {
+                    item.InWatchlist = true;
+                    item.Rating = m.Rating;
+                    item.Watched = m.Watched;
+                }
+            }
+
+            return View(model);
+        }
+
+
+        //Returns a JsonResult instead of a view(To update the DOM for the Index page without having to relad the entire page)
+        //no change - returns -1 
+        //if movie is removed - returns 0
+        //if the movie is added - returns 1
+        [HttpGet]
+        public async Task<JsonResult> AddRemove(int val, int id)
+        {
+            int retval = -1;
+            var userId = await GetCurrentUserId();
+            if (val == 1)
+            {
+                // if a record exists in UserMovies that contains both the user’s
+                // and movie’s Ids, then the movie is in the watchlist and can
+                // be removed
+                var movie = _context.UserMovies.FirstOrDefault(x =>
+                    x.MovieId == id && x.UserId == userId);
+                if (movie != null)
+                {
+                    _context.UserMovies.Remove(movie);
+                    retval = 0;
+                }
+
+            }
+            else
+            {
+                // the movie is not currently in the watchlist, so we need to
+                // build a new UserMovie object and add it to the database
+                _context.UserMovies.Add(
+                    new UserMovie
+                    {
+                        UserId = userId,
+                        MovieId = id,
+                        Watched = false,
+                        Rating = 0
+                    }
+                );
+                retval = 1;
+            }
+            // now we can save the changes to the database
+            await _context.SaveChangesAsync();
+            // and our return value (-1, 0, or 1) back to the script that called
+            // this method from the Index page
+            return Json(retval);
         }
 
         // GET: Movies/Details/5
@@ -52,6 +143,11 @@ namespace Watchlist.Controllers
         // POST: Movies/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //By default all model properties being posted from a form are bound to the corresponding controller action through the binding syntax
+        //Dynamic model binding
+        //1. Retrieves submitted data from URL routes,form fields and query strings
+        //2. Delivers the data to the controller action via method parameters.
+        //3. Converts the string data to the appropriate .NET data types.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Year")] Movie movie)
